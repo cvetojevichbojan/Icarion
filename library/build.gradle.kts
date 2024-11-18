6d8f3898651901260f0cfde47ca74afa5135340c
Bojan Cvetojevic <cvetojevichbojan@gmail.com>
2024-11-18 20:42:35 +0100

    Add publish instructions for github actions

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,8 +1,12 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlinJvm)
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 group = "xyz.amplituhedron"
+val artifactId = "icarion"
 version = "1.0"
 
 kotlin {
@@ -25,4 +29,31 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.2")
 
     testImplementation(kotlin("test"))
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    coordinates(group.toString(), artifactId, version.toString())
+
+    pom {
+        name = "Icarion library"
+        description = "Extensible app data migration"
+        inceptionYear = "2024"
+        url = "https://github.com/cvetojevichbojan/Icarion"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        scm {
+            url = "https://github.com/cvetojevichbojan/Icarion"
+            connection = "scm:git:git://github.com/cvetojevichbojan/Icarion.git"
+            developerConnection = "scm:git:ssh://git@github.com/cvetojevichbojan/Icarion.git"
+        }
+    }
 }
